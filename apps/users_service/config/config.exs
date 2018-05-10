245dc65d7fb2d6cc02eb2572a7907c73f463ebf7
josediaz16 <jldiaz1612@gmail.com>
2018-05-10 13:39:01 -0500

    adds users table

--- apps/users_service/config/config.exs
+++ apps/users_service/config/config.exs
@@ -6,7 +6,7 @@ config :users_service, UsersService.Repo,
   adapter: Ecto.Adapters.Postgres,
   database: "users_service_repo",
   username: "postgres",
-  #password: "pass",
+  password: "postgres",
   hostname: "localhost"
 
 config :users_service, ecto_repos: [UsersService.Repo]
