b10f1a9c4bfdefb3ec53897eb30bbf2490932e77
josediaz16 <jldiaz1612@gmail.com>
2018-05-10 17:21:36 -0500

    adds test implementation for users model

---
+++ apps/users_service/lib/users_service/user.ex
@@ -0,0 +1,35 @@
+defmodule UsersService.User do
+  use Ecto.Schema
+
+  schema "users" do
+    field :name,                  :string
+    field :email,                 :string
+    field :password_hash,         :string
+    field :password,              :string, virtual: true
+    field :password_confirmation, :string, virtual: true
+    field :phone_number,          :string
+    field :country,               :string
+    field :city,                  :string
+    field :address,               :string
+  end
+
+  def changeset(user, params \\ %{}) do
+    user
+      |> Ecto.Changeset.cast(params, [:name, :email, :password, :phone_number, :country, :city, :address])
+      |> Ecto.Changeset.validate_required([:name, :email, :password, :phone_number])
+      |> Ecto.Changeset.validate_format(:email, ~r/@/)
+      |> Ecto.Changeset.validate_confirmation(:password)
+      |> hash_password
+  end
+
+  defp hash_password(changeset) do
+    case changeset do
+      %Ecto.Changeset{valid?: true, changes: %{password: password}} ->
+        Ecto.Changeset.put_change(changeset,
+          :password_hash,
+          Comeonin.Bcrypt.hashpwsalt(password)
+        )
+        _ -> changeset
+    end
+  end
+end
--- apps/users_service/priv/repo/migrations/20180510181034_create_users.exs
+++ apps/users_service/priv/repo/migrations/20180510181034_create_users.exs
@@ -5,7 +5,7 @@ defmodule UsersService.Repo.Migrations.CreateUsers do
     create table(:users) do
       add :name, :string, null: false, default: ""
       add :email, :string, null: false, default: ""
-      add :password, :string, null: false, default: ""
+      add :password_hash, :string, null: false, default: ""
       add :phone_number, :string, null: false, default: ""
       add :country, :string, null: false, default: ""
       add :city, :string, null: false, default: ""
